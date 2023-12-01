555ee14d841af5ae77a4b5bd4f94ca751b178dd6
lredwine <lar348@msstate.edu>
2023-12-01 16:58:32 -0500

    Added Git Ignore

--- Final_Projects/Topic_1/Topic_1c.m
+++ Final_Projects/Topic_1/Topic_1c.m
@@ -63,33 +63,6 @@ NDVI_P = max((NIR_P - R_P) ./ (NIR_P + R_P), 0);
 original_image = im2gray(uint8(rescale(cat(3, R, G, B), 0, 255)));
 pca_pansharpened_image = im2gray(uint8(rescale(cat(3, R_P, B_P, G_P), 0, 255)));
 
-figure;
-subplot(2, 4, 1);
-imshow(cat(3, R, zeros(m, n), zeros(m, n)));
-title(['Red']);
-subplot(2, 4, 2);
-imshow(cat(3, zeros(m, n), G, zeros(m, n)));
-title(['Green']);
-subplot(2, 4, 3);
-imshow(cat(3, zeros(m, n), zeros(m, n), B));
-title(['Blue']);
-subplot(2, 4, 4);
-imshow(cat(3, NIR, zeros(m, n), zeros(m, n)));
-title(['NIR']);
-subplot(2, 4, 5);
-imshow(cat(3, R_P, zeros(m, n), zeros(m, n)));
-title(['Red']);
-subplot(2, 4, 6);
-imshow(cat(3, zeros(m, n), G_P, zeros(m, n)));
-title(['Green']);
-subplot(2, 4, 7);
-imshow(cat(3, zeros(m, n), zeros(m, n), B_P));
-title(['Blue']);
-subplot(2, 4, 8);
-imshow(cat(3, NIR_P, zeros(m, n), NIR_P));
-title(['NIR']);
-
-
 % -------------------------------------------------------------------------
 
 multispectral_bands = multispectral_images{1,1};
@@ -106,6 +79,56 @@ brovey_NDVI = ((fused_band4) - (fused_band3)) ./ ((fused_band4) + (fused_band3))
 
 % -------------------------------------------------------------------------
 
+spatial_simularity_pca = pca_pansharpened_image - original_image;
+[spatial_simularity_ssim_pca, ssimmap_pca] = ssim(double(pca_pansharpened_image), double(original_image));
+
+spatial_simularity_bro = fused_image - original_image;
+[spatial_simularity_ssim_bro, ssimmap_bro] = ssim(double(fused_image), double(original_image));
+
+pca_image = uint8(rescale(cat(3, R_P, B_P, G_P), 0, 255));
+brovey_image = uint8(rescale(cat(3, fused_band1, fused_band2, fused_band3), 0, 255));
+starting_image = uint8(rescale(cat(3, R, G, B), 0, 255));
+
+pca_reshaped = reshape(pca_image, m*n*3, 1);
+brovey_reshaped = reshape(brovey_image, m*n*3, 1);
+starting_reshaped = reshape(starting_image, m*n*3, 1);
+
+pca_data = (pca_reshaped'.*starting_reshaped)./(abs(pca_reshaped).*abs(starting_reshaped));
+pca_min = min(min(pca_data));
+pca_max = max(max(pca_data));
+scaled_pca_data = (pca_data - pca_min) / (pca_max - pca_min);
+isreal(scaled_pca_data)
+pca_spectral_simularity = acos(scaled_pca_data);
+brovey_spectral_simularity = acos((brovey_reshaped'.*starting_reshaped)./(abs(brovey_reshaped).*abs(starting_reshaped)));
+
+% -------------------------------------------------------------------------
+
+figure;
+subplot(2, 4, 1);
+imshow(cat(3, R, zeros(m, n), zeros(m, n)));
+title(['Red']);
+subplot(2, 4, 2);
+imshow(cat(3, zeros(m, n), G, zeros(m, n)));
+title(['Green']);
+subplot(2, 4, 3);
+imshow(cat(3, zeros(m, n), zeros(m, n), B));
+title(['Blue']);
+subplot(2, 4, 4);
+imshow(cat(3, NIR, NIR, NIR));
+title(['NIR']);
+subplot(2, 4, 5);
+imshow(cat(3, R_P, zeros(m, n), zeros(m, n)));
+title(['Red']);
+subplot(2, 4, 6);
+imshow(cat(3, zeros(m, n), G_P, zeros(m, n)));
+title(['Green']);
+subplot(2, 4, 7);
+imshow(cat(3, zeros(m, n), zeros(m, n), B_P));
+title(['Blue']);
+subplot(2, 4, 8);
+imshow(cat(3, NIR_P, NIR_P, NIR_P));
+title(['NIR']);
+
 figure;
 subplot(1, 4, 1);
 imshow(original_image, []);
@@ -120,7 +143,6 @@ subplot(1, 4, 4);
 imshow(fused_image);
 title(['Brovey Method Pansharpened Image']);
 
-
 figure;
 subplot(1, 3, 1);
 imshow(NDVI);
@@ -132,3 +154,16 @@ subplot(1, 3, 3);
 imshow(brovey_NDVI);
 title(['Brovey Method NDVI Image'])
 
+figure;
+subplot(2, 2, 1);
+imshow(spatial_simularity_pca, []);
+title('Spatial Simularity PCA Differential');
+subplot(2, 2, 2);
+imshow(ssimmap_pca, []);
+title(spatial_simularity_ssim_pca);
+subplot(2, 2, 3);
+imshow(spatial_simularity_pca, []);
+title('Spatial Simularity Brovey Differential');
+subplot(2, 2, 4);
+imshow(ssimmap_bro, []);
+title(spatial_simularity_ssim_bro);
